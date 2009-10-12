d9f4aa9369794ff989461811f1166c649ca9350d
chomats <chomats@imac-de-s-c.local>
2009-10-12 14:12:12 +0200

    refactoring import cadse for write a test

--- src-gen/fr/imag/adele/cadse/cadseg/operation/ImportCadsePagesAction.java
+++ src-gen/fr/imag/adele/cadse/cadseg/operation/ImportCadsePagesAction.java
@@ -21,6 +21,7 @@ package fr.imag.adele.cadse.cadseg.operation;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -204,7 +205,7 @@ public class ImportCadsePagesAction extends AbstractActionPage {
 				return true;
 			}
 			try {
-				cadse = readCadse(file);
+				cadse = ImportCadseUtil.readCadse(file);
 				if (cadse == null) {
 					setMessageError("Select a valid cadse zip file");
 					return true;
@@ -271,63 +272,14 @@ public class ImportCadsePagesAction extends AbstractActionPage {
 	 * @return the d choose file ui
 	 */
 	public DChooseFileUI createImportField() {
+		
 		return new DChooseFileUI("selectJar", "Select cadse deployed zip", EPosLabel.left, new MC_Import(),
 				new IC_Import(), "Select cadse deployed zip");
 	}
 
-	/**
-	 * Read cadse.
-	 * 
-	 * @param f
-	 *            the f
-	 * 
-	 * @return the string
-	 * 
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 * @throws JAXBException
-	 *             the JAXB exception
-	 */
-	public String readCadse(File f) throws IOException, JAXBException {
-		JarFile jis = new JarFile(f);
-		ZipEntry entry = jis.getEntry(ExportCadsePagesAction.MELUSINE_DIR_CADSENAME);
-		if (entry == null) {
-			entry = jis.getEntry("/" + ExportCadsePagesAction.MELUSINE_DIR_CADSENAME);
-			if (entry == null) {
-				throw new IOException("Cannot found " + ExportCadsePagesAction.MELUSINE_DIR_CADSENAME);
-			}
-		}
-		InputStream imput = jis.getInputStream(entry);
-		BufferedReader isr = new BufferedReader(new InputStreamReader(imput));
-		return isr.readLine();
-	}
+	
 
-	/**
-	 * Read cadse uuid.
-	 * 
-	 * @param f
-	 *            the f
-	 * 
-	 * @return the compact uuid
-	 * 
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 * @throws JAXBException
-	 *             the JAXB exception
-	 */
-	public CompactUUID readCadseUUID(File f) throws IOException, JAXBException {
-		JarFile jis = new JarFile(f);
-		ZipEntry entry = jis.getEntry(ExportCadsePagesAction.MELUSINE_DIR_CADSENAME_ID);
-		if (entry == null) {
-			entry = jis.getEntry("/" + ExportCadsePagesAction.MELUSINE_DIR_CADSENAME_ID);
-			if (entry == null) {
-				throw new IOException("Cannot found " + ExportCadsePagesAction.MELUSINE_DIR_CADSENAME_ID);
-			}
-		}
-		InputStream imput = jis.getInputStream(entry);
-		BufferedReader isr = new BufferedReader(new InputStreamReader(imput));
-		return new CompactUUID(isr.readLine());
-	}
+	
 
 	/**
 	 * Gets the select jar.
@@ -362,160 +314,10 @@ public class ImportCadsePagesAction extends AbstractActionPage {
 	@Override
 	public void doFinish(Object monitor) throws Exception {
 		super.doFinish(monitor);
-		IProgressMonitor pmo = (IProgressMonitor) monitor;
-		CadseCore.getCadseDomain().beginOperation("Import cadse");
-		try {
-			File f = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
-			File pf = new File(f, cadse);
-			ZipUtil.unzipFile(file, pf);
-			File melusineDir = new File(pf, ".melusine-dir");
-			File[] filesserxml = melusineDir.listFiles();
-			Collection<URL> itemdescription = new ArrayList<URL>();
-			for (File fser : filesserxml) {
-				if (fser.getName().endsWith(".ser")) {
-					itemdescription.add(fser.toURI().toURL());
-				}
-			}
-			Collection<ProjectAssociation> projectAssociationSet = new ArrayList<ProjectAssociation>();
-			ProjectAssociation pa = new ProjectAssociation(readCadseUUID(file), cadse);
-			projectAssociationSet.add(pa);
-			LogicalWorkspaceTransaction transaction = CadseCore.getLogicalWorkspace().createTransaction();
-
-			transaction.loadItems(itemdescription);
-			migrate(transaction);
-			transaction.commit(false, true, false, projectAssociationSet);
-			checkAction(transaction);
-		} catch (RuntimeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			CadseCore.getCadseDomain().endOperation();
-		}
+		ImportCadseUtil.importCadse(cadse, new FileInputStream(file));	
 	}
 
-	private void checkAction(LogicalWorkspaceTransaction transaction) {
-		Collection<ItemDelta> operations = transaction.getItemOperations();
-		LogicalWorkspace lw = CadseCore.getLogicalWorkspace();
-		for (ItemDelta itemDelta : operations) {
-			Item gI = lw.getItem(itemDelta.getId());
-			if (gI == null) {
-				System.err.println("Cannot found commited item "+itemDelta);
-				continue;
-			}
-			Item parent = gI.getPartParent();
-			if (parent == null && itemDelta.getPartParent() != null) {
-				System.err.println("Parent not setted "+itemDelta+" -> "+itemDelta.getPartParent());
-			} else {
-				if (parent != null && itemDelta.getPartParent() != null) {
-					if (!parent.getId().equals(itemDelta.getPartParent().getId())) {
-						System.err.println("Parent not same "+itemDelta+" -> "+itemDelta.getPartParent()+"<>"+parent);
-					}
-				}
-			}
-				
-		}
-		
-	}
-
-	private void migrate(LogicalWorkspaceTransaction transaction) throws CadseException {
-	Collection<ItemDelta> operations = transaction.getItemOperations();
-		for (ItemDelta itemDelta : operations) {
-			
-			
-			if (itemDelta.getType() == null) {
-				if (itemDelta.getBaseItem() != null) {
-					itemDelta.setType(itemDelta.getBaseItem().getType());
-				}
-				else
-					System.out.println("type has no type "+itemDelta);
-			}
-			
-			if (!itemDelta.isLoaded())
-				continue;
-			
-			SetAttributeOperation uuid_att = itemDelta.getSetAttributeOperation("UUID_ATTRIBUTE");
-			if (uuid_att != null) {
-				if (itemDelta.getType() == null) {
-					System.out.println("Import erroor type is null");
-				} else if (itemDelta.getType() == CadseGCST.CADSE_DEFINITION) {
-					itemDelta.setAttribute(CadseGCST.CADSE_DEFINITION_at_ID_RUNTIME_, uuid_att.getCurrentValue());
-				} else if (itemDelta.isInstanceOf(CadseGCST.PAGE)) {
-					itemDelta.setAttribute(CadseGCST.PAGE_at_ID_RUNTIME_, uuid_att.getCurrentValue());
-				} else if (itemDelta.isInstanceOf(CadseGCST.ATTRIBUTE)) {
-					itemDelta.setAttribute(CadseGCST.ATTRIBUTE_at_ID_RUNTIME_, uuid_att.getCurrentValue());
-				} else if (itemDelta.isInstanceOf(CadseGCST.ABSTRACT_ITEM_TYPE)) {
-					itemDelta.setAttribute(CadseGCST.ABSTRACT_ITEM_TYPE_at_ID_RUNTIME_, uuid_att.getCurrentValue());
-				} else {
-					System.out.println("Cannot set UUID_ATTRIBUTE for type "+itemDelta.getType().getName());
-				}
-				//remove old valeur
-				itemDelta.setAttribute("UUID_ATTRIBUTE", null);
-			}
-			if (itemDelta.getType() == CadseGCST.LINK) {
-				if (itemDelta.getName().startsWith("#invert_part")) {
-					itemDelta.delete(false);
-					for(Link l : itemDelta.getIncomingLinks()) {
-						l.delete();
-					}
-				}
-				LinkDelta l = itemDelta.getOutgoingLink(CadseGCST.LINK_lt_INVERSE_LINK);
-				if (l != null && l.getDestination().getName().startsWith("#invert_part")) {
-					l.delete();
-				}
-			}
-			SetAttributeOperation committed_date_value = itemDelta.getSetAttributeOperation(CadseGCST.ITEM_at_COMMITTED_DATE);
-			if (committed_date_value != null) {
-				if (committed_date_value.getCurrentValue() instanceof Date) {
-					Date d = (Date) committed_date_value.getCurrentValue();
-					itemDelta.setAttribute(CadseGCST.ITEM_at_COMMITTED_DATE_, d.getTime());
-				}
-			}
-		}
-		
-		for (ItemDelta itemDelta : operations) {
-			if (!itemDelta.isLoaded())
-				continue;
-			for (LinkDelta l : itemDelta.getOutgoingLinkOperations()) {
-				if (l.getLinkTypeName().startsWith("#parent:") ||
-						l.getLinkTypeName().startsWith("#invert_part") ) {
-					if (itemDelta.getPartParent() == null) {
-						itemDelta.setParent(l.getDestination(), null);
-					}
-					if (getOutgoingLink(CadseGCST.ITEM_lt_PARENT) == null) {
-						itemDelta.createLink(CadseGCST.ITEM_lt_PARENT, l.getDestination());
-					}
-					l.delete();
-				}
-				if (l.getLinkType() != null && l.getLinkType().isPart() && l.getDestination().getPartParent() == null) {
-					l.getDestination().setParent(l.getSource(), l.getLinkType());
-				}
-			}
-			for (LinkDelta l : itemDelta.getOutgoingLinkOperations(CadseGCST.ITEM_lt_MODIFIED_ATTRIBUTES)) {
-				if (l.getDestination().getType() == null) {
-					IAttributeType<?> att = l.getSource().getType().getAttributeType(l.getDestinationName(), false);
-					
-					 if (att != null) {
-						LinkDelta latt = itemDelta.getOutgoingLink(CadseGCST.ITEM_lt_MODIFIED_ATTRIBUTES,att.getId());
-						if (latt != null) {
-							l.delete();
-						} else
-							l.changeDestination(att);
-					 }
-						
-					else 
-						l.delete();
-				}
-			}
-		}
-		for (ItemDelta itemDelta : operations) {
-			if (!itemDelta.isLoaded())
-				continue;
-			if (itemDelta.getPartParent() == null && itemDelta.getType() != null && itemDelta.getType().isPartType()) {
-				System.out.println("Error cannot found parent for "+itemDelta.getQualifiedName());
-			}
-		}
-	}
-
+	
 	/**
 	 * Gets the file.
 	 * 
