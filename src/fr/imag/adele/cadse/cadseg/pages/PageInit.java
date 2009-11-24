239f42ed0060cdabbc78f661a1d54a314623c06f
chomats <chomats@localhost>
2009-11-24 16:34:26 +0100

    add name field

--- src/fr/imag/adele/cadse/cadseg/pages/PageInit.java
+++ src/fr/imag/adele/cadse/cadseg/pages/PageInit.java
@@ -2,7 +2,11 @@ package fr.imag.adele.cadse.cadseg.pages;
 
 import fr.imag.adele.cadse.cadseg.validators.JavaPackageValidator;
 import fr.imag.adele.cadse.core.CadseGCST;
+import fr.imag.adele.cadse.core.CompactUUID;
 import fr.imag.adele.cadse.core.Item;
+import fr.imag.adele.cadse.core.impl.ui.UIFieldImpl;
+import fr.imag.adele.cadse.core.impl.ui.mc.MC_Descriptor;
+import fr.imag.adele.cadse.core.ui.EPosLabel;
 
 public class PageInit {
 
@@ -16,6 +20,10 @@ public class PageInit {
 		CadseGCST.ITEM_lt_INSTANCE_OF.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, false);
 		
 		CadseGCST.ITEM_at_NAME_.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, true);
+		
+		CadseGCST.ITEM.addField(new UIFieldImpl(CadseGCST.DTEXT, CompactUUID.randomUUID(),CadseGCST.ITEM_at_NAME_,"name",EPosLabel.left,
+				new MC_Descriptor(CadseGCST.MC_NAME_ATTRIBUTE), null));
+		
 		CadseGCST.CADSE_DEFINITION_at_PACKAGENAME_.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, true);
 		CadseGCST.CADSE_lt_EXTENDS.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, true);
 		JavaClassValidator v  = new JavaClassValidator();
