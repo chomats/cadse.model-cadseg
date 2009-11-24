c43e07c491af7842f196e6f4480049b4dce2806e
chomats <chomats@localhost>
2009-11-24 10:43:46 +0100

    (no commit message)

--- src/fr/imag/adele/cadse/cadseg/pages/PageInit.java
+++ src/fr/imag/adele/cadse/cadseg/pages/PageInit.java
@@ -2,10 +2,22 @@ package fr.imag.adele.cadse.cadseg.pages;
 
 import fr.imag.adele.cadse.cadseg.validators.JavaPackageValidator;
 import fr.imag.adele.cadse.core.CadseGCST;
+import fr.imag.adele.cadse.core.Item;
 
 public class PageInit {
 
 	public static void init() {
+		CadseGCST.CADSE_DEFINITION_lt_BUILD.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, false);
+		CadseGCST.CADSE_DEFINITION_lt_CONFIGURATION.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, false);
+		CadseGCST.CADSE_DEFINITION_lt_DATA_MODEL.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, false);
+		CadseGCST.CADSE_DEFINITION_lt_MAPPING.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, false);
+		CadseGCST.CADSE_DEFINITION_lt_VIEW_MODEL.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, false);
+		
+		CadseGCST.ITEM_lt_INSTANCE_OF.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, false);
+		
+		CadseGCST.ITEM_at_NAME_.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, true);
+		CadseGCST.CADSE_DEFINITION_at_PACKAGENAME_.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, true);
+		CadseGCST.CADSE_lt_EXTENDS.setFlag(Item.MUST_BE_INITIALIZED_AT_CREATION_TIME, true);
 		JavaClassValidator v  = new JavaClassValidator();
 		v.setClazz(JavaPackageValidator.class);
 		v.setListenAttributes(CadseGCST.CADSE_DEFINITION_at_PACKAGENAME_);
