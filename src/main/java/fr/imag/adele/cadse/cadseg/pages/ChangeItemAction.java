9e6de72fc0886fb234d5e651d42706c2d60e1a48
chomats <chomats@localhost>
2010-01-15 07:55:56 +0100

    (no commit message)

--- src/main/java/fr/imag/adele/cadse/cadseg/pages/ChangeItemAction.java
+++ src/main/java/fr/imag/adele/cadse/cadseg/pages/ChangeItemAction.java
@@ -1,6 +1,8 @@
 package fr.imag.adele.cadse.cadseg.pages;
 
 import fr.imag.adele.cadse.cadseg.managers.CadseDefinitionManager;
+import fr.imag.adele.cadse.cadseg.managers.content.ManagerManager;
+import fr.imag.adele.cadse.cadseg.managers.dataModel.ItemTypeManager;
 import fr.imag.adele.cadse.core.CadseException;
 import fr.imag.adele.cadse.core.CadseGCST;
 import fr.imag.adele.cadse.core.Item;
@@ -9,12 +11,14 @@ import fr.imag.adele.cadse.core.attribute.IAttributeType;
 import fr.imag.adele.cadse.core.impl.ui.PageImpl;
 import fr.imag.adele.cadse.core.ui.IActionPage;
 import fr.imag.adele.cadse.core.ui.IPage;
+import fr.imag.adele.cadse.core.ui.Pages;
 import fr.imag.adele.cadse.core.ui.UIField;
 import fr.imag.adele.cadse.core.ui.UIPlatform;
 
 public class ChangeItemAction implements IActionPage {
 
 	IPage _page;
+    private UIPlatform _uiPlatform;
 	public ChangeItemAction(IPage page) {
 		_page = page;
 	}
@@ -34,8 +38,9 @@ public class ChangeItemAction implements IActionPage {
 	@Override
 	public void doFinish(UIPlatform uiPlatform, Object monitor)
 			throws Exception {
-		// TODO Auto-generated method stub
-
+//		if (!initManager_) {
+//			initManager(null);
+//		}
 	}
 
 	@Override
@@ -46,17 +51,21 @@ public class ChangeItemAction implements IActionPage {
 
 	@Override
 	public void init(UIPlatform uiPlatform) throws CadseException {
-		Item manager = null;
-		if (!uiPlatform.isModification()) {
-			Item baseItem = uiPlatform.getItem();
-			Item cadseDef = baseItem.getPartParent(CadseGCST.CADSE_DEFINITION);
-			Item mapping = CadseDefinitionManager.getMapping(cadseDef);
-			manager  = uiPlatform.getCopy().createItem(CadseGCST.MANAGER, mapping, CadseGCST.MAPPING_MODEL_lt_MANAGERS);
-		} else {
-			Item baseItem = uiPlatform.getItem();
-			manager  = baseItem.getIncomingItem(CadseGCST.MANAGER_lt_ITEM_TYPE);
-		}
-		associateItem(uiPlatform, manager);
+                _uiPlatform = uiPlatform;
+        theitemtype = uiPlatform.getItem();
+        managerItem = theitemtype.getIncomingItem(CadseGCST.MANAGER_lt_ITEM_TYPE);
+
+//		Item manager = null;
+//		if (!uiPlatform.isModification()) {
+//			Item baseItem = uiPlatform.getItem();
+//			Item cadseDef = baseItem.getPartParent(CadseGCST.CADSE_DEFINITION);
+//			Item mapping = CadseDefinitionManager.getMapping(cadseDef);
+//			manager  = uiPlatform.getCopy().createItem(CadseGCST.MANAGER, mapping, CadseGCST.MAPPING_MODEL_lt_MANAGERS);
+//		} else {
+//			Item baseItem = uiPlatform.getItem();
+//			manager  = baseItem.getIncomingItem(CadseGCST.MANAGER_lt_ITEM_TYPE);
+//		}
+		associateItem(uiPlatform, managerItem);
 	}
 
 	private void associateItem(UIPlatform uiPlatform, Item manager) {
@@ -72,5 +81,62 @@ public class ChangeItemAction implements IActionPage {
 		// TODO Auto-generated method stub
 
 	}
+
+	/** The manager item. */
+	private Item	managerItem;
+
+	/** The theitemtype. */
+	private Item	theitemtype;
+
+	/** The init manager_. */
+	private boolean	initManager_	= false;	;
+
+//	/*
+//	 * (non-Javadoc)
+//	 *
+//	 * @see fr.imag.adele.cadse.core.impl.ui.AbstractActionPage#doNextPageAction(java.lang.Object,
+//	 *      fr.imag.adele.cadse.core.ui.Pages, int)
+//	 */
+//	@Override
+//	public void doNextPageAction(Object monitor, Pages pages, int currentPage) throws Exception {
+//		if (currentPage == 0) {
+//			if (!initManager_) {
+//				initManager(pages);
+//			}
+//		}
+//		super.doNextPageAction(monitor, pages, currentPage);
+//	}
+//
+//	/**
+//	 * Inits the manager.
+//	 *
+//	 * @param pages
+//	 *            the pages
+//	 *
+//	 * @throws CadseException
+//	 *             the melusine exception
+//	 */
+//	private void initManager(Pages pages) throws CadseException {
+//		if (pages != null) {
+//			_uiPlatform.updateField(CadseGCST.MANAGER_at_HUMAN_NAME_);
+//		}
+//
+//		Item superItem = ItemTypeManager.getSuperType(theitemtype);
+//		if (superItem != null) {
+//			Item supermanager = ManagerManager.getManagerFromItemType(superItem);
+//			if (supermanager != null) {
+//				ManagerManager.setUniqueNameTemplate(managerItem,
+//						ManagerManager.getUniqueNameTemplate(supermanager));
+//				ManagerManager.setDisplayNameTemplateAttribute(managerItem,
+//						ManagerManager
+//								.getDisplayNameTemplateAttribute(supermanager));
+//				if (pages != null) {
+//					_uiPlatform.updateField(
+//							CadseGCST.MANAGER_at_QUALIFIED_NAME_TEMPLATE_);
+//				}
+//			}
+//		}
+//		initManager_ = true;
+//	}
 
 }
