e78d95ae6dac475564550d271d8ffc27de126490
Thomas <Thomas@localhost>
2010-02-05 16:22:35 +0100

    computation of impacts of update operations

---
+++ src/main/java/fr/imag/adele/cadse/cadseg/teamwork/update/UpdateImpactUtil.java
@@ -0,0 +1,75 @@
+package fr.imag.adele.cadse.cadseg.teamwork.update;
+
+import java.util.List;
+import java.util.UUID;
+
+import fr.imag.adele.cadse.core.Item;
+import fr.imag.adele.cadse.core.LinkType;
+import fr.imag.adele.cadse.core.Link;
+import fr.imag.adele.cadse.core.LogicalWorkspace;
+import fr.imag.adele.cadse.core.impl.internal.TWUtil;
+
+/**
+ * Methods used to compute list of operation to perform 
+ * from requirements of an update operation.
+ * 
+ * @author Thomas
+ *
+ */
+public class UpdateImpactUtil {
+
+	/**
+	 * Computes impacts of requirements.
+	 * 
+	 * @param updateState update state containing update operation definition.
+	 */
+	public static void computeImpacts(UpdateState updateState) {
+		UpdateDefinition definition = updateState.getDefinition();
+		definition.clearImpacts();
+		
+		for (Operation op : definition.getRequirements()) {
+			computeImpact(op, updateState);
+		}
+	}
+
+	private static void computeImpact(Operation requirementOp, UpdateState updateState) {
+		UUID itemId = requirementOp.getItemId();
+		List<Operation> impacts = updateState.getDefinition().getImpacts();
+		LogicalWorkspace lw = updateState.getTransaction();
+		
+		// add this operation
+		Operation impactOp = requirementOp.clone();
+		impactOp.addCause(requirementOp);
+		updateState.getDefinition().getImpacts().add(impactOp);
+		
+		Item item = null;
+		if (!impactOp.isImport()) {
+			item = lw.getItem(impactOp.getItemId());
+		}
+		
+		// manage coupled propagation
+		if (impactOp.isRevert()) {
+			RevertOperation revertOp = (RevertOperation) impactOp;
+			
+			for (Link link : item.getIncomingLinks()) {
+				if (!TWUtil.isCoupled(link.getLinkType()))
+					continue;
+				
+				Item srcItem = link.getSource();
+				RevertOperation srcImpactOp = new RevertOperation(srcItem.getId(), false);
+				srcImpactOp.addCause(impactOp);
+				updateState.getDefinition().getImpacts().add(impactOp);
+			}
+			for (Link link : item.getOutgoingLinks()) {
+				if (!TWUtil.isCoupled(link.getLinkType()))
+					continue;
+				
+				Item destItem = link.getDestination();
+				RevertOperation destImpactOp = new RevertOperation(destItem.getId(), false);
+				destImpactOp.addCause(impactOp);
+				updateState.getDefinition().getImpacts().add(destImpactOp);
+			}
+		}
+	}
+
+}
