d35177e4076c275396b822fc1c1c5ae5b4920213
etienne <etienne@localhost>
2010-02-15 17:58:01 +0100

    validator for extends class initialization

---
+++ src/main/java/fr/imag/adele/cadse/cadseg/validators/ExtendsClassValidator.java
@@ -0,0 +1,29 @@
+package fr.imag.adele.cadse.cadseg.validators;
+
+import fr.imag.adele.cadse.core.CadseException;
+import fr.imag.adele.cadse.core.CadseGCST;
+import fr.imag.adele.cadse.core.ui.AbstractUIRunningValidator;
+import fr.imag.adele.cadse.core.ui.UIPlatform;
+
+public class ExtendsClassValidator extends AbstractUIRunningValidator {
+
+
+	@Override
+	public void initAfterUI() {
+		// TODO Auto-generated method stub
+		super.initAfterUI();
+	}
+	
+	
+	@Override
+ 	public void init(UIPlatform uiPlatform) {
+ 		super.init(uiPlatform);
+ 		
+ 		if (!uiPlatform.isModification())
+			try {
+				uiPlatform.getItem().setAttribute(CadseGCST.CONTENT_ITEM_TYPE_at_EXTENDS_CLASS_, true);
+			} catch (CadseException e) {
+				e.printStackTrace();
+			}
+ 	}
+}
