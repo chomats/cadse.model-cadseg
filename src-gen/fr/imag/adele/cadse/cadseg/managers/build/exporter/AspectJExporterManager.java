bcde24fa6ceceb9b97b23889a4f3d31e9d90a457
Stephane Chomat <Stephane.Chomat@imag.fr>
2010-02-17 12:09:07 +0100

    add accessors

--- src-gen/fr/imag/adele/cadse/cadseg/managers/build/exporter/AspectJExporterManager.java
+++ src-gen/fr/imag/adele/cadse/cadseg/managers/build/exporter/AspectJExporterManager.java
@@ -1,6 +1,8 @@
 package fr.imag.adele.cadse.cadseg.managers.build.exporter;
 
+import fr.imag.adele.cadse.core.CadseGCST;
 import fr.imag.adele.cadse.core.Item;
+import fr.imag.adele.cadse.core.ItemType;
 import fr.imag.adele.cadse.core.LinkType;
 
 /**
