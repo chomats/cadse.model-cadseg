4a4680348604d3bc36c6be2cf743f99e033830de
Stephane Chomat <Stephane.chomat@imag.fr>
2010-07-07 16:42:21 +0200

    fix list

---
+++ src/main/java/fr/imag/adele/cadse/cadseg/pages/mc/MC_DestTypeCombo.java
@@ -0,0 +1,130 @@
+package fr.imag.adele.cadse.cadseg.pages.mc;
+
+import java.util.List;
+
+import fr.imag.adele.cadse.core.CadseException;
+import fr.imag.adele.cadse.core.CadseGCST;
+import fr.imag.adele.cadse.core.ChangeID;
+import fr.imag.adele.cadse.core.Item;
+import fr.imag.adele.cadse.core.ItemType;
+import fr.imag.adele.cadse.core.Link;
+import fr.imag.adele.cadse.core.LinkType;
+import fr.imag.adele.cadse.core.attribute.EnumAttributeType;
+import fr.imag.adele.cadse.core.attribute.IAttributeType;
+import fr.imag.adele.cadse.core.impl.CadseCore;
+import fr.imag.adele.cadse.core.impl.CadseIllegalArgumentException;
+import fr.imag.adele.cadse.core.impl.ui.mc.ItemLinkTypeWorkspaceListener;
+import fr.imag.adele.cadse.core.impl.ui.mc.MC_AttributesItem;
+import fr.imag.adele.cadse.core.oper.WSODeleteLink;
+import fr.imag.adele.cadse.core.transaction.delta.ItemDelta;
+import fr.imag.adele.cadse.core.ui.UIField;
+import fr.imag.adele.cadse.core.ui.UIPlatform;
+
+public class MC_DestTypeCombo extends MC_AttributesItem {
+
+	private boolean					mandatory	= false;
+	private String					msg			= null;
+	boolean							init;
+	private ItemDelta secondItem;
+//	Object defaultValue = null;
+	
+	
+	
+	
+
+	@Override
+	protected Object modelToVisual(Object ret) {
+		if (ret == null) return null;
+		IAttributeType<?> attRef = getUIField().getAttributeDefinition();
+		if (attRef.getType() == CadseGCST.LINK_TYPE) {
+			LinkType lt = (LinkType)attRef;
+			List<Link> ret2 = (List<Link>) ret;
+			if (lt.getMax() == 1) {
+				return ret2.size() >= 1 ? ret2.get(0) : null;
+			}
+			return ret;
+		}
+		return ret;
+	}
+	
+
+	@Override
+	public Object getValue() {
+		Item item = _uiPlatform.getItem(getUIField());
+		if (item == null) {
+			throw new CadseIllegalArgumentException("No item in the context.");
+		}
+		IAttributeType<?> attRef = getUIField().getAttributeDefinition();
+		if (attRef.getType() == CadseGCST.LINK_TYPE) {
+			LinkType lt = (LinkType)attRef;
+			Link ret = item.getOutgoingLink(lt);
+			if (ret == null)
+				return null;
+			return ret.getDestinationType();
+		}
+			
+		return null;
+	}
+
+
+	@Override
+	public void notifieValueChanged(UIField field, Object value) {
+		if (secondItem != null)
+			try {
+				secondItem.delete(false);
+			} catch (CadseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		try {
+			secondItem = _uiPlatform.openInTransactionDialog("sub-element",
+					_uiPlatform.getItem(), (LinkType) getUIField().getAttributeDefinition(), (ItemType)value,
+					CadseGCST.ITEM_at_NAME_, CadseGCST.ATTRIBUTE_at_DEFAULT_VALUE_, 
+					CadseGCST.ATTRIBUTE_at_SHOW_IN_DEFAULT_CP_,
+					CadseGCST.ATTRIBUTE_at_SHOW_IN_DEFAULT_MP_);
+		} catch (CadseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean validValue(UIField field, Object value) {
+		if (field == getUIField()) {
+			if (value instanceof ItemType) {
+				return false;
+			}
+			_uiPlatform.setMessageError("Select sub type");
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean validValueChanged(UIField field, Object value) {
+		if (value instanceof ItemType) {
+			return false;
+		}
+		_uiPlatform.setMessageError("Select sub type");
+		return true;
+	}
+
+	@Override
+	public void notifieValueDeleted(UIField field, Object oldvalue) {
+		if (oldvalue instanceof Link) {
+			Link l = (Link) oldvalue;
+			WSODeleteLink oper = new WSODeleteLink(l);
+			oper.execute();
+			CadseCore.registerInTestIfNeed(oper);
+		}
+	}
+
+	public ItemType getType() {
+		return CadseGCST.MC_LINK;
+	}
+
+	@Override
+	public Object defaultValue() {
+		return null;
+	}
+}
