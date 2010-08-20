fa0ea6612231f7b523f3484fb330163e4fa17e0e
Stephane Chomat <Stephane.chomat@imag.fr>
2010-08-20 15:03:20 +0200

    fix compilation for content mananagement began the separation between non-eclipse part and eclipse-part

---
+++ src-gen/fr/imag/adele/cadse/cadseg/contents/MenuActionContent.java
@@ -0,0 +1,21 @@
+package fr.imag.adele.cadse.cadseg.contents;
+
+import java.util.UUID;
+
+import fede.workspace.eclipse.java.manager.JavaFileContentManager;
+import fr.imag.adele.cadse.core.CadseException;
+import fr.imag.adele.cadse.core.impl.var.NullVariable;
+
+/**
+		@generated
+	*/
+ public class MenuActionContent extends JavaFileContentManager {
+
+		/**
+			@generated
+		*/
+		public MenuActionContent(UUID id) throws CadseException {
+			super(id, NullVariable.INSTANCE, NullVariable.INSTANCE);
+		}
+
+	}
